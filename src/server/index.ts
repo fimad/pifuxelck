e779c0a1540caf0b060e4e126b9567ed35dc94e9
Will Coster <willcoster@gmail.com>
2020-03-13 20:49:03 -0700

    Fix winston logging in deploy

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,15 @@
 require('@google-cloud/trace-agent').start();
 
+import { LoggingWinston } from '@google-cloud/logging-winston';
+import * as functions from 'firebase-functions';
 import * as winston from 'winston';
 
 import config from '../../config/prod';
 import server from './server';
 
-const LoggingWinston: any = require('@google-cloud/logging-winston');
-const functions: any = require('firebase-functions');
-
-winston.add(LoggingWinston);
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [new winston.transports.Console(), new LoggingWinston()],
+});
 
 exports.app = functions.https.onRequest(server(config));
